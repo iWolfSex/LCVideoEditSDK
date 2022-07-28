1d0735b67d85c92349ccfd9c1732c9f938e86265
iWolf <727388890@qq.com>
2022-07-28 12:43:38 +0800

    add flie

---
+++ LCVideoEditSDK.framework/Headers/LCAadd.h
@@ -0,0 +1,17 @@
+//
+//  LCAadd.h
+//  LCVideoEditSDK
+//
+//  Created by iWolf on 2022/7/26.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LCAadd : NSObject
+-(NSInteger)AddNumbera:(NSInteger)numbera withNumberb:(NSInteger)numnerb;
+
+@end
+
+NS_ASSUME_NONNULL_END
